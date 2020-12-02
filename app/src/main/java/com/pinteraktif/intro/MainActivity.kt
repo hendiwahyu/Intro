fef6956fcd674259f7d373f3b67c7006f3a69cb9
Hendi Wahyu <hendiwahyu@pinteraktif.com>
2020-12-02 07:28:45 +0700

    Initial commit

---
+++ app/src/main/java/com/pinteraktif/intro/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.pinteraktif.intro
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tittle: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tittle = findViewById(R.id.tittle_tv)
+        button = findViewById(R.id.main_btn)
+
+
+        button.setOnClickListener {
+            tittle.visibility = View.VISIBLE
+        }
+    }
+}
